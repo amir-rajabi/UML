cbc808a0b67c2ef718bf4249a03a25a4b33b7f6e
Feliks <you@yourdomain.example.com>
2023-06-18 00:21:58 +0200

    forgot to add testing to commit

--- ml_utils/testing.py
+++ ml_utils/testing.py
@@ -5,10 +5,16 @@ from ml_utils.model import ConvolutionalNeuralNetwork
 #from model import ConvolutionalNeuralNetwork
 import os
 
-def test_drawing():
+def test_drawing(revert):
     # Lade das trainierte Modell
     model = ConvolutionalNeuralNetwork(dropout_rate=0)  # Passe den dropout_rate-Wert entsprechend an
-    model.load_state_dict(torch.load('data/model.pt'))
+    if not revert and os.path.exits('data/model_new.pt'):
+        model.load_state_dict(torch.load('data/model_new.pt'))
+    elif os.path.exits('data/model.pt'):
+        model.load_state_dict(torch.load('data/model.pt'))
+    else: 
+        print("LOG: there is no model to evaluate")
+
     model.eval()
 
     # Lade und transformiere das Bild
