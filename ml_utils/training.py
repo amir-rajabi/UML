60f47c379afe1498dc8b0f5a2b402d3e94e2eb3c
Feliks <you@yourdomain.example.com>
2023-06-23 19:10:50 +0200

    one hot encoding and more loss functions

--- ml_utils/training.py
+++ ml_utils/training.py
@@ -19,42 +19,29 @@ from ml_utils.model import ConvolutionalNeuralNetwork
 from ml_utils.json_write import write_json, get_run_num 
 from ml_utils.print_overwrite import print
 
-loss = [F.cross_entropy, F.multi_margin_loss, F.nll_loss]
+#cross_entroyp - soft_margin_loss good
+#l1_loss weird
+#smooth_l1_loss decent
+#poisson_nll_loss decent
+
+#
+loss_func = [F.cross_entropy, F.multi_margin_loss, F.multilabel_soft_margin_loss,
+        F.soft_margin_loss, F.l1_loss, F.smooth_l1_loss, F.poisson_nll_loss]
 stop_flag = False
 
 
 def train_step(model: Module, optimizer: Optimizer, data: Tensor,
-               target: Tensor, cuda: bool, loss_func):
+               target: Tensor, cuda: bool, loss_nr):
     model.train()
     if cuda:
         data, target = data.cuda(), target.cuda()
     prediction = model(data)
-    #NOTE: size of targe is [256]
-    #       size of prediction is [256,10]
-    #TODO: add function to adjust loss function
-    #   will probably also have to adjust size
-    #   depending on the loss function
 
-    #NOTE: for testing other loss functions
-    #print("Target\n")
-    #print(target)
-    #print(target.size())
-    #print("Prediction\n")
-    #print(prediction)
-    #print(prediction.size())
-    #time.sleep(30)
-    #print("batch")
+    if loss_nr > 1:
+        target = F.one_hot(target)
 
-    #NOTE: likely needs one_hot_encoding
-    #loss = F.multilabel_margin_loss(prediction, target)
-    #loss = F.multilabel_soft_margin_loss(prediction, target)
-    #loss = F.cosine_embedding_loss(prediction, target)
-    #loss = F.hinge_embedding_loss(prediction, target)
-    #loss = F.poisson_nll_loss(prediction, target)
-    #loss = F.gaussian_nll_loss(prediction, target)
-    
     #currently uses loss_function intput into the function
-    loss = loss_func(prediction, target)
+    loss = loss_func[loss_nr](prediction, target)
     loss.backward()
     optimizer.step()
     optimizer.zero_grad()
@@ -63,7 +50,7 @@ def train_step(model: Module, optimizer: Optimizer, data: Tensor,
 def training(chart_data, socketio, dictionary, model: Module,
             optimizer: Optimizer,
             cuda: bool, n_epochs: int,
-            batch_size: int, loss_func):
+            batch_size: int, loss_nr):
     global_epoch=len(chart_data["d1"])
     run_index = get_run_num()
     train_loader, test_loader = get_data_loaders(batch_size=batch_size)
@@ -73,7 +60,7 @@ def training(chart_data, socketio, dictionary, model: Module,
         for batch in train_loader:
             data, target = batch
             train_step(model=model, optimizer=optimizer, cuda=cuda, data=data,
-                       target=target, loss_func=loss_func)
+                       target=target, loss_nr=loss_nr)
             if stop_flag:
                 break
         else:
@@ -148,7 +135,7 @@ def start_training(data, socketio, params):
         cuda=False,
         n_epochs=int(params["epochs"]),
         batch_size=int(params["batch_size"]),
-        loss_func=loss[int(params["loss_function"])]
+        loss_nr=int(params["loss_function"])
     )
 
 #is called by frontend when interrupt ist set
@@ -156,3 +143,4 @@ def stop_training():
     global stop_flag 
     stop_flag = True
     print('LOG: STOPPED')
+
