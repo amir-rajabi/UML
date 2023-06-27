e9603d209afd5d40daa1f6bca443bed60437c7ae
Benedikt Schmitz <benedikt.schmitz@arcor.de>
2023-06-27 14:46:14 +0200

    Add possibility to join first and second diagram

--- static/a_js/dia.js
+++ static/a_js/dia.js
@@ -4,8 +4,11 @@ import {createAlert} from './alerts.js';
 window.chartData = chartData;
 window.socket = socket;
 
+var dia1ctr = document.getElementById('dia1-ctr');
 var dia2ctr = document.getElementById('dia2-ctr');
+var dia3ctr = document.getElementById('dia3-ctr');
 var dia2switch = document.getElementById('flexSwitchCheckDefault');
+var dia3join = document.getElementById('joinDiagramsSwitch');
 var secondcardbody = document.getElementById('second-card-body');
 
 var selector1 = document.querySelector('.adjust-dropdown1');
@@ -16,20 +19,51 @@ var selected1 = '0';
 var selected2 ='1';
 var labels = ['Accuracy (test-set)', 'Loss (test-set)', 'Accuracy (train-set)', 'Loss (train-set)'];
 var second = false;
+var joined = false;
 
 //--------------- set displaying content ---------------//
 dia2switch.addEventListener('click', function(){
   if (dia2switch.checked == true){
     second = true;
     saveDataToSessionStorage();
-    dia2ctr.style.display = 'block';
+    if (joined) {
+      dia1ctr.style.display = 'none';
+      dia2ctr.style.display = 'none';
+      dia3ctr.style.display = 'block';
+    } else {
+      dia2ctr.style.display = 'block';
+      dia3ctr.style.display = 'none';
+    }
     secondcardbody.style.display = 'block';
   }
   else if (dia2switch.checked == false){
     second = false;
     saveDataToSessionStorage();
+    dia1ctr.style.display = 'block';
     dia2ctr.style.display = 'none';
+    dia3ctr.style.display = 'none';
     secondcardbody.style.display = 'none';
+  }
+});
+
+dia3join.addEventListener('click', function(){
+  if (dia3join.checked == true){
+    joined = true;
+    saveDataToSessionStorage();
+    dia1ctr.style.display = 'none';
+    dia2ctr.style.display = 'none';
+    dia3ctr.style.display = 'block';
+  }
+  else if (dia3join.checked == false){
+    joined = false;
+    saveDataToSessionStorage();
+    dia1ctr.style.display = 'block';
+    if (second) {  
+      dia2ctr.style.display = 'block';
+    } else {
+      dia2ctr.style.display = 'none';
+    }
+    dia3ctr.style.display = 'none';
   }
 });
 
@@ -37,6 +71,7 @@ dia2switch.addEventListener('click', function(){
 window.addEventListener('resize', function() {
   dia1.resize();
   dia2.resize();
+  dia3.resize();
 });
 //--------------- dia 1 ---------------//
 const dia1 = new Chart(
@@ -78,6 +113,30 @@ const dia2 = new Chart(
     }
   }
 );
+//--------------- dia 3 ---------------//
+const dia3 = new Chart(
+  document.getElementById('dia3'),
+  {
+    type: 'line',
+    data: {
+      labels: selected_data1.map((_, index) => `Epoch ${index + 1}`),
+      datasets: [
+        {
+          label: labels[0],
+          data: selected_data1
+        },
+        {
+          label: labels[1],
+          data: selected_data2
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: true
+    }
+  }
+);
 
 //--------------- updater ---------------//
 var first_alert = 0;
@@ -95,6 +154,7 @@ socket.on('update_chart', function(data){
 
   updateChart(selected1, dia1);
   updateChart(selected2, dia2);
+  updateChart(selected1, dia3, selected2);
 });
 
 //--------------- progressbar ---------------//
@@ -123,17 +183,20 @@ socket.on('update_progress', function(data){
 selector1.addEventListener("change", function(event) {
   selected1 = event.target.value;
   updateChart(selected1, dia1);
+  updateChart(selected1, dia3, selected2);
   saveDataToSessionStorage();
 });
 
 selector2.addEventListener("change", function(event) {
   selected2 = event.target.value;
   updateChart(selected2, dia2);
+  updateChart(selected1, dia3, selected2);
   saveDataToSessionStorage();
 });
 
-function updateChart(selectedValue, dia){
+function updateChart(selectedValue, dia, secondSelectedValue = -1){
   var selected;
+  var secondSelected;
   
   if (selectedValue === '0') {
     selected = chartData.d1;
@@ -170,6 +233,42 @@ function updateChart(selectedValue, dia){
     dia.data.datasets[0].label = labels[3];
     dia.update();
   }
+  
+  if (secondSelectedValue === '0') {
+    secondSelected = chartData.d1;
+    dia.data.labels = secondSelected.map(function(_, index) {
+      return 'Epoch ' + (index + 1);
+    });
+    dia.data.datasets[1].data = secondSelected;
+    dia.data.datasets[1].label = labels[0];
+    dia.update();
+  } 
+  else if (secondSelectedValue === '1') {
+    secondSelected = chartData.d2;
+    dia.data.labels = secondSelected.map(function(_, index) {
+      return 'Epoch ' + (index + 1);
+    });
+    dia.data.datasets[1].data = secondSelected;
+    dia.data.datasets[1].label = labels[1];
+    dia.update();
+  } 
+  else if (secondSelectedValue === '2') {
+    secondSelected = chartData.d3;
+    dia.data.labels = secondSelected.map(function(_, index) {
+      return 'Epoch ' + (index + 1);
+    });
+    dia.data.datasets[1].data = secondSelected;
+    dia.data.datasets[1].label = labels[2];
+    dia.update();
+  } else if (secondSelectedValue === '3') {
+    secondSelected = chartData.d4;
+    dia.data.labels = secondSelected.map(function(_, index) {
+      return 'Epoch ' + (index + 1);
+    });
+    dia.data.datasets[1].data = secondSelected;
+    dia.data.datasets[1].label = labels[3];
+    dia.update();
+  }
 }
 
 //--------------- safe-data to session storager ---------------//
@@ -178,6 +277,7 @@ function saveDataToSessionStorage() {
   sessionStorage.setItem('selected1', selected1);
   sessionStorage.setItem('selected2', selected2);
   sessionStorage.setItem('second', second);
+  sessionStorage.setItem('joined', joined);
 }
 
 window.addEventListener('DOMContentLoaded', function() {
@@ -191,12 +291,24 @@ window.addEventListener('DOMContentLoaded', function() {
     selected2 = sessionStorage.getItem('selected2');
     selector2.value = selected2;
     updateChart(selected2, dia2);
+    updateChart(selected1, dia3, selected2)
   }
   
+  if (sessionStorage.getItem('joined')){
+    joined = sessionStorage.getItem('joined');
+    dia3join.checked = joined === 'true'; // Convert the value to a boolean
+  }
+
   if (sessionStorage.getItem('second')){
     second = sessionStorage.getItem('second');
     dia2switch.checked = second === 'true'; // Convert the value to a boolean
-    dia2ctr.style.display = second ? 'block' : 'none';
+    if (joined) {
+      dia1ctr.style.display = 'none';
+      dia2ctr.style.display = 'none';
+      dia3ctr.style.display = 'block';
+    } else {  
+      dia2ctr.style.display = second ? 'block' : 'none';
+    }
     secondcardbody.style.display = second ? 'block' : 'none';
   }
 });
