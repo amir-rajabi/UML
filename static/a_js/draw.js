21aac64d9fe718536f7eee26a05a6c5f2af97b6f
kocadae01 <kocadae01@zedat.fu-berlin.de>
2023-07-02 19:30:06 +0200

    revised testing menu: cursor is now a pen, placeholder for the picture of the model. captions added

--- static/a_js/draw.js
+++ static/a_js/draw.js
@@ -11,16 +11,29 @@ canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', stopDrawing);
 canvas.addEventListener('mouseleave', stopDrawing);
 
+var predict_button = document.getElementById('predict_drawing');
+var clear_button = document.getElementById('clear_drawing');
+var predicted_no = document.getElementById('predicted_no');
+
+// -------------------- FUNCTIONS
+
+function stopDrawing() {
+    drawing = false;
+}
+function clear_canvas(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    predicted_no.innerHTML = '&nbsp;';
+}
 function startDrawing(event) {
     drawing = true;
     var { offsetX, offsetY } = event;
     previousX = offsetX;
     previousY = offsetY;
-}
 
+    predict_button.removeAttribute('disabled');
+}
 function draw(event) {
     if (!drawing) return;
-
     var { offsetX, offsetY } = event;
     context.beginPath();
     context.moveTo(previousX, previousY);
@@ -29,33 +42,22 @@ function draw(event) {
     context.strokeStyle = 'white';
     context.lineCap = "round";
     context.stroke();
-
     previousX = offsetX;
     previousY = offsetY;
 }
 
-function stopDrawing() {
-    drawing = false;
-}
+// -------------------- EVENTLISTENER
 
-var predicted_no = document.getElementById('predicted_no');
-
-document.getElementById('clear_drawing').addEventListener('click', function(){
+clear_button.addEventListener('click', function(){
     clear_canvas();
+    predict_button.setAttribute('disabled','');
 });
 
-function clear_canvas(){
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    predicted_no.textContent = "";
-}
-
-document.getElementById('predict_drawing').addEventListener('click', function(){
+predict_button.addEventListener('click', function(){
     var imageData = canvas.toDataURL();
-
     var xhr = new XMLHttpRequest();
     xhr.open('POST', '/predict_drawing', true);
     xhr.setRequestHeader('Content-Type', 'application/json');
-
     xhr.onload = function() {
         if (xhr.status === 200) {
             var response = JSON.parse(xhr.responseText);
