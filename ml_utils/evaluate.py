d9091ec86227c9d91e1f0c39010bc00515d91179
Amir Rajabi <amir.rajabi@fu-berlin.de>
2023-08-07 01:34:16 +0200

    Savinf False detected images as PNG in a specific Folder

--- ml_utils/evaluate.py
+++ ml_utils/evaluate.py
@@ -4,6 +4,9 @@
 # module:        SWP - Usable Machine Learning
 # year:          2023
 
+# evaluate.py
+
+import os
 import numpy as np
 import torch
 import torch.nn.functional as F
@@ -12,11 +15,12 @@ from torch.autograd import Variable
 from torch.nn import Module
 from torch.utils.data import DataLoader
 from ml_utils.progressbar import send_pb
+from PIL import Image
 
 stop_flag_eval = False
 
 loss_func = [F.cross_entropy, F.multi_margin_loss, F.multilabel_soft_margin_loss,
-        F.soft_margin_loss, F.l1_loss, F.smooth_l1_loss, F.poisson_nll_loss]
+             F.soft_margin_loss, F.l1_loss, F.smooth_l1_loss, F.poisson_nll_loss]
 
 def accuracy(loss_nr, model: Module, loader: DataLoader, cuda: bool) -> (float, float):
     model.eval()
@@ -24,28 +28,47 @@ def accuracy(loss_nr, model: Module, loader: DataLoader, cuda: bool) -> (float, 
     bcounter = 0
     correct = 0
     batches = len(loader)
+
+    # Create a directory to save the false detected images
+    output_dir = "false_detected_images"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
     with torch.no_grad():
         for data, target in loader:
             if bcounter % 10 == 0:
-                #see progressbar module
-                send_pb(batches, bcounter/batches)
+                # see progressbar module
+                send_pb(batches, bcounter / batches)
             bcounter += 1
             global stop_flag_eval
             if stop_flag_eval:
                 stop_flag_eval = False
-                return -1,-1
+                return -1, -1
             if cuda:
                 data, target = data.cuda(), target.cuda()
             data, target = Variable(data), Variable(target)
             output = model(data)
             target_input = target
             if loss_nr > 1:
-                target_input = F.one_hot(target,10)
+                target_input = F.one_hot(target, 10)
             losses.append(loss_func[loss_nr](output, target_input).item())
             pred = output.data.max(1, keepdim=True)[1]
             correct += pred.eq(target.data.view_as(pred)).cpu().sum().item()
+
+            # Save false detected images
+            for i in range(len(target)):
+                if pred[i] != target[i]:
+                    image = data[i].cpu().numpy().squeeze()
+                    image = (image * 255).astype(np.uint8)
+                    image = Image.fromarray(image)
+                    label = target[i].item()
+                    prediction = pred[i].item()
+                    image_name = f"{output_dir}/{label}_as_{prediction}_{i}.png"
+                    image.save(image_name)
+
     eval_loss = float(np.nanmean(losses))
     return eval_loss, 100. * correct / len(loader.dataset)
+
 
 
 def accuracy_per_class(model: Module, loader: DataLoader, cuda: bool) \
@@ -78,3 +101,4 @@ def init_eval_flag():
 def stop_eval():
     global stop_flag_eval
     stop_flag_eval = True
+
--- ml_utils/training.py
+++ ml_utils/training.py
@@ -20,7 +20,7 @@ from flask_socketio import SocketIO
 from ml_utils.data import get_data_loaders
 from ml_utils.evaluate import accuracy, init_eval_flag
 from ml_utils.model import ConvolutionalNeuralNetwork
-from ml_utils.json_write import write_json, get_run_num 
+from ml_utils.json_write import write_json, get_run_num
 from ml_utils.print_overwrite import print
 from ml_utils.progressbar import init_pb, update_pb_epoch, send_pb
 
@@ -28,10 +28,11 @@ start_timer = 0
 stop_flag = False
 
 loss_func = [F.cross_entropy, F.multi_margin_loss, F.multilabel_soft_margin_loss,
-        F.soft_margin_loss, F.l1_loss, F.smooth_l1_loss, F.poisson_nll_loss]
+             F.soft_margin_loss, F.l1_loss, F.smooth_l1_loss, F.poisson_nll_loss]
+
 
 def train_step(model: Module, optimizer: Optimizer, data: Tensor,
-               target: Tensor, cuda: bool, loss_nr, false_detected_samples):
+               target: Tensor, cuda: bool, loss_nr):
     model.train()
     if cuda:
         data, target = data.cuda(), target.cuda()
@@ -45,27 +46,18 @@ def train_step(model: Module, optimizer: Optimizer, data: Tensor,
     optimizer.step()
     optimizer.zero_grad()
 
-    # Identify false detected samples and save them
-    predicted_labels = prediction.argmax(dim=1)
-    false_detections = data[predicted_labels != target]
-    false_detected_samples.extend(false_detections)
-
 
 def training(name, chart_data, socketio, dictionary, model: Module,
-            optimizer: Optimizer,
-            cuda: bool, n_epochs: int,
-            batch_size: int, loss_nr):
-
-    #for progressbar
-    batches = math.ceil(60000/batch_size)
+             optimizer: Optimizer,
+             cuda: bool, n_epochs: int,
+             batch_size: int, loss_nr):
+    # for progressbar
+    batches = math.ceil(60000 / batch_size)
     init_pb(socketio, batch_size, n_epochs)
 
-    global_epoch=len(chart_data["d1"])
+    global_epoch = len(chart_data["d1"])
     run_index = get_run_num(f"data/{name}_epoch_data.json")
     train_loader, test_loader = get_data_loaders(batch_size=batch_size)
-
-    false_detected_samples = []
-
     if cuda:
         model.cuda()
     for epoch in range(n_epochs):
@@ -74,19 +66,19 @@ def training(name, chart_data, socketio, dictionary, model: Module,
         for batch in train_loader:
             data, target = batch
             train_step(model=model, optimizer=optimizer, cuda=cuda, data=data,
-                       target=target, loss_nr=loss_nr, false_detected_samples=false_detected_samples)
+                       target=target, loss_nr=loss_nr)
             if bcounter % 10 == 0:
-                #see progressbar module
-                send_pb(batches, bcounter/batches)
+                # see progressbar module
+                send_pb(batches, bcounter / batches)
             bcounter += 1
 
             if stop_flag:
                 break
         else:
-            #gets triggered if inner loop clean finishes
-            #doesn't get triggered if inner loop breaks
+            # gets triggered if inner loop clean finishes
+            # doesn't get triggered if inner loop breaks
 
-            #send_pb updates pb but flat without fraction calc
+            # send_pb updates pb but flat without fraction calc
 
             send_pb(-1, 0.46)
             test_loss, test_accuracy = accuracy(loss_nr, model, test_loader, cuda)
@@ -95,17 +87,17 @@ def training(name, chart_data, socketio, dictionary, model: Module,
                 break
             send_pb(-1, 0.53)
 
-            train_loss,train_accuracy = accuracy(loss_nr, model, train_loader, cuda)
+            train_loss, train_accuracy = accuracy(loss_nr, model, train_loader, cuda)
 
-            send_pb(-1,1)
+            send_pb(-1, 1)
             if stop_flag:
                 break
 
-            #triggered on first epoch to check time through logs
+            # triggered on first epoch to check time through logs
             if not epoch:
-                print(f"LOG: time for one epoch: {time.time()-start_timer}")
+                print(f"LOG: time for one epoch: {time.time() - start_timer}")
 
-            #detecting overflow of loss
+            # detecting overflow of loss
             if str(test_loss) == "nan":
                 test_loss = 10
                 print("LOG: Overflow detected in testing loss")
@@ -113,8 +105,8 @@ def training(name, chart_data, socketio, dictionary, model: Module,
                 train_loss = 10
                 print("LOG: Overflow detected in training loss")
 
-            #if model overflows the accuracy will be 9.8
-            #checking for overflow if that is the case
+            # if model overflows the accuracy will be 9.8
+            # checking for overflow if that is the case
             if int(test_accuracy) < 10:
                 is_nan = torch.stack([torch.isnan(p).any() for p in model.parameters()]).any()
                 if is_nan:
@@ -130,19 +122,21 @@ def training(name, chart_data, socketio, dictionary, model: Module,
             chart_data['d3'].append(train_accuracy)
             chart_data['d4'].append(train_loss)
             chart_data['run'].append(str(run_index))
-            socketio.emit('update_chart', {'data':chart_data})
+            socketio.emit('update_chart', {'data': chart_data})
             print("LOG: update chart")
 
             dictionary["loss"] = str(test_loss)
             dictionary["accuracy"] = str(test_accuracy)
             dictionary["train_loss"] = str(train_loss)
             dictionary["train_accuracy"] = str(train_accuracy)
-            dictionary["run"]=str(run_index)
-            write_json(dictionary,path=f"data/{name}_epoch_data.json")
+            dictionary["run"] = str(run_index)
+            write_json(dictionary, path=f"data/{name}_epoch_data.json")
             curr_glob_epoch = len(chart_data["d1"])
 
-            print(f'LOG: epoch={curr_glob_epoch}/{global_epoch+n_epochs}, train accuracy={train_accuracy}, train loss={train_loss}')
-            print(f'LOG: epoch={curr_glob_epoch}/{global_epoch+n_epochs}, test accuracy={test_accuracy}, test loss={test_loss}')
+            print(
+                f'LOG: epoch={curr_glob_epoch}/{global_epoch + n_epochs}, train accuracy={train_accuracy}, train loss={train_loss}')
+            print(
+                f'LOG: epoch={curr_glob_epoch}/{global_epoch + n_epochs}, test accuracy={test_accuracy}, test loss={test_loss}')
 
             torch.save(model.state_dict(), f'data/{name}_model_new.pt')
             continue
@@ -153,30 +147,30 @@ def training(name, chart_data, socketio, dictionary, model: Module,
         print('LOG: STOPPED')
     else:
         print("LOG: TRAINING FINISHED")
-    socketio.emit('training_finished', {'data':chart_data})
+    socketio.emit('training_finished', {'data': chart_data})
 
-#init model
-def init_model(name,model):
+
+# init model
+def init_model(name, model):
     if os.path.exists(f"data/{name}_model.pt"):
         model.load_state_dict(torch.load(f"data/{name}_model.pt"))
         print(f"LOG:{name} model loaded")
     return model
 
 
-#this function is used by frontend
-#socket to JS frontend
-#data is the current graph in JS
-#that will be updated by training
+# this function is used by frontend
+# socket to JS frontend
+# data is the current graph in JS
+# that will be updated by training
 def start_training(name, data, socketio, params):
-    
-    seed = ((len(data["run"]) << 3)*31)%256
+    seed = ((len(data["run"]) << 3) * 31) % 256
     manual_seed(seed)
     np.random.seed(seed)
     model = ConvolutionalNeuralNetwork(float(params["dropout_rate"]))
-    model = init_model(name,model)
-    opt = SGD(model.parameters(), lr=float(params["learning_rate"]), 
+    model = init_model(name, model)
+    opt = SGD(model.parameters(), lr=float(params["learning_rate"]),
               momentum=float(params["momentum"]))
-    
+
     global stop_flag
     stop_flag = False
     init_eval_flag()
@@ -199,16 +193,19 @@ def start_training(name, data, socketio, params):
         loss_nr=int(params["loss_function"])
     )
 
-#is called by frontend when interrupt ist set
+
+# is called by frontend when interrupt ist set
 def stop_training():
-    global stop_flag 
+    global stop_flag
     stop_flag = True
 
-#style 1-normal; 2-success; 3-danger; 4-warning
-#could import from webserver because of cyclic dependency
+
+# style 1-normal; 2-success; 3-danger; 4-warning
+# could import from webserver because of cyclic dependency
 def sendAlert(style, content, socketio):
     data = {
         'style': style,
         'content': content
     }
-    socketio.emit('sendAlert', {'data':data})   
+    socketio.emit('sendAlert', {'data': data})
+
